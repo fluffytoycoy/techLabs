325d19e51825f36f6ccc7b476841ac8a5e9c0714
Matt Coy <fluffytoycoy@gmail.com>
2019-08-21 11:49:39 -0700

    grid done animation added moving on to info section

--- front/src/components/HomePage/Home.js
+++ front/src/components/HomePage/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import "animate.css/animate.min.css";
+import ScrollAnimation from 'react-animate-on-scroll';
 import './Home.scss';
 
 function Home(){
@@ -11,11 +13,17 @@ function Home(){
             <h1>data solutions</h1>
             <h1>innovation </h1>
           </div>
-          <div className="grid">
-            <div className="clouds"></div>
-            <div className="data"></div>
-            <div className="science"></div>
-            <div className="community"></div>
+        </section>
+        <section className="grid">
+            <ScrollAnimation animateOnce animateIn="fadeInUp"><div className="clouds"><h3>Cloud Services</h3></div></ScrollAnimation>
+            <ScrollAnimation animateOnce delay={100} animateIn="fadeInUp"><div className="data"><h3>Data Engineering</h3></div></ScrollAnimation>
+            <ScrollAnimation animateOnce delay={200}animateIn="fadeInUp"><div className="science"><h3>Decision Science</h3></div></ScrollAnimation>
+            <ScrollAnimation animateOnce delay={300}animateIn="fadeInUp"><div className="community"><h3>Community Driven</h3></div></ScrollAnimation>
+        </section>
+        <section className="info">
+          <div className="picture-col">
+          </div>
+          <div className="info-col">
           </div>
         </section>
       </div>
