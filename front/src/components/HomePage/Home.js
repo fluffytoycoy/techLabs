3271326d32079eac4795e6d15e981302b296b4e6
Matt Coy <fluffytoycoy@gmail.com>
2019-08-21 09:20:47 -0700

    added grid to hero

--- front/src/components/HomePage/Home.js
+++ front/src/components/HomePage/Home.js
@@ -5,7 +5,19 @@ import './Home.scss';
 function Home(){
     return (
       <div id="home">
-        home
+        <section id="hero">
+          <div className="jumbo">
+            <h1>modern software</h1>
+            <h1>data solutions</h1>
+            <h1>innovation </h1>
+          </div>
+          <div className="grid">
+            <div className="clouds"></div>
+            <div className="data"></div>
+            <div className="science"></div>
+            <div className="community"></div>
+          </div>
+        </section>
       </div>
     );
 }
