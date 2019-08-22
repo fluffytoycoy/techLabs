f188e9740c78728c85d170369472b8305fc0066d
Matt Coy <fluffytoycoy@gmail.com>
2019-08-22 00:28:25 -0700

    hero styled new effects added to header

---
+++ front/src/components/HomePage/Hero/Hero.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import './Hero.scss'
+
+
+
+function Hero(){
+  return(
+    <section id="hero">
+      <div className="jumbo">
+        <div className="img-wrapper">
+          <img src="/img/labs_logo.png"/>
+        </div>
+        <div className="header-col col">
+            <div>
+              <div className="title">
+                <h1><span>Develope specialized software solutions</span><br/>
+                From proof of concept to ready to deploy solutions</h1>
+              </div>
+            </div>
+        </div>
+        <div className="map-col col">
+          <div>
+            <div className="title">
+              <h1>1904 WAS A YEAR THAT BROUGHT TREMENDOUS PASSION, TALENT, AND
+              INNOVATION TO ST LOUIS with the worldfair. WIth this mind set
+              we inspire those same qualities in innovated ways</h1>
+              <Button variant="outlined">Contact Us</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero;
--- front/src/components/HomePage/Home.js
+++ front/src/components/HomePage/Home.js
@@ -2,26 +2,27 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import "animate.css/animate.min.css";
 import ScrollAnimation from 'react-animate-on-scroll';
+import Button from '@material-ui/core/Button';
+import Hero from './Hero/Hero';
 import './Home.scss';
 
 function Home(){
     return (
       <div id="home">
-        <section id="hero">
-          <div className="jumbo">
-            <h1>modern software</h1>
-            <h1>data solutions</h1>
-            <h1>innovation </h1>
-          </div>
-        </section>
-        <section className="grid">
-            <ScrollAnimation animateOnce animateIn="fadeInUp"><div className="clouds"><h3>Cloud Services</h3></div></ScrollAnimation>
-            <ScrollAnimation animateOnce delay={100} animateIn="fadeInUp"><div className="data"><h3>Data Engineering</h3></div></ScrollAnimation>
-            <ScrollAnimation animateOnce delay={200}animateIn="fadeInUp"><div className="science"><h3>Decision Science</h3></div></ScrollAnimation>
-            <ScrollAnimation animateOnce delay={300}animateIn="fadeInUp"><div className="community"><h3>Community Driven</h3></div></ScrollAnimation>
-        </section>
+        <Hero/>
+        <div className="grid-wrapper">
+          <section className="grid">
+              <ScrollAnimation offset={50} animateOnce animateIn="fadeInUp"><div className="clouds"><h3>Cloud Services</h3></div></ScrollAnimation>
+              <ScrollAnimation offset={50} animateOnce delay={100} animateIn="fadeInUp"><div className="data"><h3>Data Engineering</h3></div></ScrollAnimation>
+              <ScrollAnimation offset={50} animateOnce delay={200}animateIn="fadeInUp"><div className="science"><h3>Decision Science</h3></div></ScrollAnimation>
+              <ScrollAnimation offset={50} animateOnce delay={300}animateIn="fadeInUp"><div className="community"><h3>Community Driven</h3></div></ScrollAnimation>
+          </section>
+        </div>
         <section className="info">
           <div className="picture-col">
+          <h1>modern software</h1>
+          <h1>data solutions</h1>
+          <h1>innovation </h1>
           </div>
           <div className="info-col">
           </div>
