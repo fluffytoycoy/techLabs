664b09599b73af84e88a44c37cd975270c14a2ac
Matt Coy <fluffytoycoy@gmail.com>
2019-08-26 03:03:15 -0700

    css clean up

--- front/src/components/HomePage/LinkGrid/LinkGrid.js
+++ front/src/components/HomePage/LinkGrid/LinkGrid.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './LinkGrid.scss';
+import {Link} from 'react-router-dom';
 import ScrollAnimation from 'react-animate-on-scroll';
 
 function LinkGrid(){
   return(
     <div className="grid-wrapper">
       <section className="grid">
-          <ScrollAnimation offset={50} animateOnce animateIn="fadeInUp"><div className="clouds"><h3>Cloud Services</h3></div></ScrollAnimation>
-          <ScrollAnimation offset={50} animateOnce delay={100} animateIn="fadeInUp"><div className="data"><h3>Data Engineering</h3></div></ScrollAnimation>
-          <ScrollAnimation offset={50} animateOnce delay={200}animateIn="fadeInUp"><div className="science"><h3>Decision Science</h3></div></ScrollAnimation>
-          <ScrollAnimation offset={50} animateOnce delay={300}animateIn="fadeInUp"><div className="community"><h3>Community Driven</h3></div></ScrollAnimation>
+          <ScrollAnimation offset={50} animateOnce animateIn="fadeInUp"><Link to="/Services/Cloud"><div className="clouds"><h3>Cloud Services</h3></div></Link></ScrollAnimation>
+          <ScrollAnimation offset={50} animateOnce delay={100} animateIn="fadeInUp"><Link to="/Services/Data-Engineering"><div className="data"><h3>Data Engineering</h3></div></Link></ScrollAnimation>
+          <ScrollAnimation offset={50} animateOnce delay={200}animateIn="fadeInUp"><Link to="/Services/Decision-Science"><div className="science"><h3>Decision Science</h3></div></Link></ScrollAnimation>
+          <ScrollAnimation offset={50} animateOnce delay={300}animateIn="fadeInUp"><Link to="/Culture/Community"><div className="community"><h3>Community Driven</h3></div></Link></ScrollAnimation>
       </section>
     </div>
   )
