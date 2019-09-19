5eb44a5003cbfc0730f20559f7a3c91530946c1e
Matt Coy <fluffytoycoy@gmail.com>
2019-09-19 12:20:17 -0700

    css on landing page

--- front/src/components/HomePage/Info/Info.js
+++ front/src/components/HomePage/Info/Info.js
@@ -3,46 +3,78 @@ import './Info.scss';
 import ScrollAnimation from 'react-animate-on-scroll';
 
 function Info(){
+  function one(){
+    return          <>
+    <div>
+                  <h2>modern software</h2>
+                  <p>We modernize legacy platforms and transform them to meet the demands of today's digital world.</p>
+                </div>
+                <div>
+                <h2>data solutions</h2>
+                <p>We Leverage data and web technology to enhance and optimized user experiences.</p>
+              </div>
+              <div>
+                <h2>Innovation</h2>
+                <p>Simply put we solve problems and save you money</p>
+              </div>
+              </>
+  }
   return(
     <section id="info">
       <div className="info-wrapper">
+      <h1>Case Studies</h1>
         <div>
           <ScrollAnimation animateOnce animateIn="fadeInLeft">
-            <div className="icon">
-              <i className="fas fa-laptop-code"></i>
+          <div>
+            <div className="circle">
             </div>
+            <div className="img-wrapper">
+              <img src="https://www.placecage.com/g/500/500"/>
+            </div>
+          </div>
           </ScrollAnimation>
           <ScrollAnimation animateOnce  animateIn="fadeInRight">
             <div>
-              <h2>modern software</h2>
-              <p>We modernize legacy platforms and transform them to meet the demands of today's digital world.</p>
+              <h2>Where does it come from?</h2>
+              <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
+              </p>
             </div>
           </ScrollAnimation>
         </div>
         <div>
           <ScrollAnimation animateOnce animateIn="fadeInLeft">
-            <div className="icon">
-              <i className="fas fa-server">  </i>
+          <div>
+            <div className="circle">
             </div>
+            <div className="img-wrapper">
+              <img src="https://www.placecage.com/g/500/500"/>
+            </div>
+          </div>
           </ScrollAnimation>
           <ScrollAnimation animateOnce animateIn="fadeInRight">
-            <div>
-              <h2>data solutions</h2>
-              <p>We Leverage data and web technology to enhance and optimized user experiences.</p>
-            </div>
+          <div>
+            <h2>Where does it come from?</h2>
+            <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
+            </p>
+          </div>
           </ScrollAnimation>
         </div>
         <div>
         <ScrollAnimation animateOnce animateIn="fadeInLeft">
-          <div className="icon">
-            <i className="far fa-lightbulb"></i>
+          <div>
+            <div className="circle">
+            </div>
+            <div className="img-wrapper">
+              <img src="https://www.placecage.com/g/500/500"/>
+            </div>
           </div>
         </ScrollAnimation>
           <ScrollAnimation animateOnce animateIn="fadeInRight">
-            <div>
-              <h2>Innovation</h2>
-              <p>Simply put we solve problems and save you money</p>
-            </div>
+          <div>
+            <h2>Where does it come from?</h2>
+            <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
+            </p>
+          </div>
           </ScrollAnimation>
         </div>
       </div>
--- front/src/components/HomePage/LinkGrid/LinkGrid.js
+++ front/src/components/HomePage/LinkGrid/LinkGrid.js
@@ -5,14 +5,14 @@ import ScrollAnimation from 'react-animate-on-scroll';
 
 function LinkGrid(){
   return(
-    <div className="grid-wrapper">
-      <section className="grid">
+    <section className="info-grid">
+      <div className="grid">
           <ScrollAnimation offset={50} animateOnce animateIn="fadeInUp"><Link to="/Services/Cloud"><div className="clouds"><h3>Cloud Services</h3></div></Link></ScrollAnimation>
           <ScrollAnimation offset={50} animateOnce delay={100} animateIn="fadeInUp"><Link to="/Services/Data-Engineering"><div className="data"><h3>Data Engineering</h3></div></Link></ScrollAnimation>
           <ScrollAnimation offset={50} animateOnce delay={200}animateIn="fadeInUp"><Link to="/Services/Decision-Science"><div className="science"><h3>Decision Science</h3></div></Link></ScrollAnimation>
-          <ScrollAnimation offset={50} animateOnce delay={300}animateIn="fadeInUp"><Link to="/Culture/Community"><div className="community"><h3>Community Driven</h3></div></Link></ScrollAnimation>
-      </section>
-    </div>
+          <ScrollAnimation offset={50} animateOnce delay={300}animateIn="fadeInUp"><Link to="/Culture/Community"><div className="community"><h3>Community</h3></div></Link></ScrollAnimation>
+      </div>
+    </section>
   )
 }
 
