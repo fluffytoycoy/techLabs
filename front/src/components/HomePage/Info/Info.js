0d42dab37ecf01ada808d8e69f56dd250ac49acb
Matt Coy <fluffytoycoy@gmail.com>
2019-09-19 17:51:36 -0700

    link grid changed to be responsive

--- front/src/components/HomePage/Info/Info.js
+++ front/src/components/HomePage/Info/Info.js
@@ -22,45 +22,9 @@ function Info(){
   return(
     <section id="info">
       <div className="info-wrapper">
-      <h1>Case Studies</h1>
-        <div>
-          <ScrollAnimation animateOnce animateIn="fadeInLeft">
-          <div>
-            <div className="circle">
-            </div>
-            <div className="img-wrapper">
-              <img src="https://www.placecage.com/g/500/500"/>
-            </div>
-          </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateOnce  animateIn="fadeInRight">
-            <div>
-              <h2>Where does it come from?</h2>
-              <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-              </p>
-            </div>
-          </ScrollAnimation>
+        <div className="grid-header">
         </div>
-        <div>
-          <ScrollAnimation animateOnce animateIn="fadeInLeft">
-          <div>
-            <div className="circle">
-            </div>
-            <div className="img-wrapper">
-              <img src="https://www.placecage.com/g/500/500"/>
-            </div>
-          </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateOnce animateIn="fadeInRight">
-          <div>
-            <h2>Where does it come from?</h2>
-            <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-            </p>
-          </div>
-          </ScrollAnimation>
-        </div>
-        <div>
-        <ScrollAnimation animateOnce animateIn="fadeInLeft">
+        <ScrollAnimation className="item1" animateOnce animateIn="fadeInLeft">
           <div>
             <div className="circle">
             </div>
@@ -69,14 +33,33 @@ function Info(){
             </div>
           </div>
         </ScrollAnimation>
-          <ScrollAnimation animateOnce animateIn="fadeInRight">
+        <ScrollAnimation className="item2" animateOnce animateIn="fadeInLeft">
           <div>
-            <h2>Where does it come from?</h2>
-            <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-            </p>
+            <div className="circle">
+            </div>
+            <div className="img-wrapper">
+              <img src="https://www.placecage.com/g/500/500"/>
+            </div>
           </div>
-          </ScrollAnimation>
-        </div>
+        </ScrollAnimation>
+        <ScrollAnimation className="item3" animateOnce animateIn="fadeInLeft">
+          <div>
+            <div className="circle">
+            </div>
+            <div className="img-wrapper">
+              <img src="https://www.placecage.com/g/500/500"/>
+            </div>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation className="item3" animateOnce animateIn="fadeInLeft">
+          <div>
+            <div className="circle">
+            </div>
+            <div className="img-wrapper">
+              <img src="https://www.placecage.com/g/500/500"/>
+            </div>
+          </div>
+        </ScrollAnimation>
       </div>
     </section>
   )
