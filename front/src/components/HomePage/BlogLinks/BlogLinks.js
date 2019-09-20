b3fdc2bfbdce5eeeafd1d8b4954b5c23fda439f1
Matt Coy <fluffytoycoy@gmail.com>
2019-09-20 14:23:17 -0700

    ccs updates to padding

--- front/src/components/HomePage/BlogLinks/BlogLinks.js
+++ front/src/components/HomePage/BlogLinks/BlogLinks.js
@@ -6,10 +6,8 @@ import ScrollAnimation from 'react-animate-on-scroll';
 function BlogLinks(){
   return(
     <section id="blog">
-      <ScrollAnimation offset={50} animateOnce animateIn="fadeInDown"><div><h1>Blog</h1></div></ScrollAnimation>
+      <ScrollAnimation offset={50} animateOnce animateIn="fadeInDown"><div><h1>Why choose us?</h1></div></ScrollAnimation>
       <section className="blog-grid">
-          <ScrollAnimation offset={50} animateOnce animateIn="fadeInLeft"><Link to="/Blog/Article/Cage-it-up"><div><div className="clouds"></div><h3>
-          Cage It Up</h3></div></Link></ScrollAnimation>
           <ScrollAnimation offset={50} animateOnce delay={100} animateIn="fadeInRight"><Link to="/Blog/Article/Modernize-Data-Management"><div><div className="data"></div><h3>Modernize Data Management</h3></div></Link></ScrollAnimation>
       </section>
     </section>
--- front/src/components/HomePage/Info/GridItem.js
+++ front/src/components/HomePage/Info/GridItem.js
@@ -10,39 +10,9 @@ function GridItem(props){
     backgroundPosition: 'center',
     backgroundSize: 'cover'
   }
-  const gridItems=[
-    {
-      id: 1,
-      title: 'The Green Fields',
-      postType: 'case study',
-      description: 'A multinational pharmaceutical and life sciences company endured tedious, time-intensive processes to compare large sets of crop trial data. We combined reporting platforms into a single solution that offered more powerful analytic capabilities and improved the overall user experience.',
-      img: '/img/HeroSquares/field.jpeg'
-    },
-    {
-      id: 2,
-      title: 'Modernized Data Management',
-      postType: 'blog',
-      description: '',
-      img: '/img/Blog/old-computer-small.png'
-    },
-    {
-      id: 3,
-      title: 'Breaking MonoLiths',
-      postType: 'case study',
-      description: 'A telecommunications client needed to scale its individual applications and services independently and deliver to production faster to provide greater business value.',
-      img: '/img/HeroSquares/mono.jpg'
-    },
-    {
-      id: 4,
-      title: 'Cage it up',
-      postType: 'blog',
-      description: '',
-      img: '/img/Blog/bubsy.gif'
-    },
-  ]
 
   return(
-    <ScrollAnimation className="item" animateOnce animateIn="fadeInLeft">
+    <ScrollAnimation className="item" animateOnce animateIn="fadeInRight">
       <div style={styles}>
         <div className="hub-info">
             <div className="title">
@@ -51,7 +21,7 @@ function GridItem(props){
             </div>
             <div className="body">
             <p>{props.item.description}</p>
-            <Link to="/#contact"><Button variant="outlined">Contact Us</Button></Link>
+            <Link to="/#contact"><Button variant="outlined">Read More</Button></Link>
           </div>
         </div>
       </div>
