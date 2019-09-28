1625268a3f152cab38dda270822d9338caa99b27
Matt Coy <fluffytoycoy@gmail.com>
2019-09-27 23:25:56 -0700

    build for coycoding subdomain testing

---
+++ public/react/precache-manifest.1f929e741cb47c7895894e6bc0b924f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000830bb3d25b762ee8bc59a214d593a",
+    "url": "./react/index.html"
+  },
+  {
+    "revision": "269c2a32a95392fe0cc8",
+    "url": "./react/static/css/2.bc959ecb.chunk.css"
+  },
+  {
+    "revision": "ff37d17fffbd02f0c94f",
+    "url": "./react/static/css/main.c43bcd75.chunk.css"
+  },
+  {
+    "revision": "269c2a32a95392fe0cc8",
+    "url": "./react/static/js/2.2cbc1075.chunk.js"
+  },
+  {
+    "revision": "ff37d17fffbd02f0c94f",
+    "url": "./react/static/js/main.8c57a5a8.chunk.js"
+  },
+  {
+    "revision": "329d88c329b94292fcbc",
+    "url": "./react/static/js/runtime~main.a75cdfa5.js"
+  }
+]);
