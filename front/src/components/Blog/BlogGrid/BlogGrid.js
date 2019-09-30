4dba8c8ca3cf6b7d03e02797e97dc6c12b964e71
Matt Coy <fluffytoycoy@gmail.com>
2019-09-29 22:42:14 -0700

    finished logic for fancy blog grid

--- front/src/components/Blog/BlogGrid/BlogGrid.js
+++ front/src/components/Blog/BlogGrid/BlogGrid.js
@@ -3,6 +3,7 @@ import ScrollAnimation from 'react-animate-on-scroll';
 import {Link} from 'react-router-dom';
 import {createUrlSlug} from '../../Utils/UrlHandlers/UrlHandlers';
 import PropTypes from 'prop-types';
+import './BlogGrid.scss';
 
 class BlogGrid extends React.Component {
     constructor(props){
@@ -17,11 +18,37 @@ class BlogGrid extends React.Component {
     }
 
     updateDimensions = () => {
-      if(window.innerWidth < 768 && this.state.width >= 768){
-        this.setState({rightCol:[]})
-      }
-      else if(window.innerWidth >= 768 && this.state.width < 768){
-        this.setState({rightCol: this.state.leftCol})
+      //If we have elements
+      if(this.props.elements){
+
+        //To desktop from ipad
+        //shift half of first 2 columns to thrid column
+        if(window.innerWidth >= 1400 && this.state.width < 1400){
+          let {leftCol, midCol, rightCol} = this.state;
+          for(let i = 0; i < Math.floor(leftCol.length/2); i++){
+            rightCol.push(leftCol.pop())
+          }
+          for(let i = 0; i < Math.floor(midCol.length/2); i++){
+            rightCol.push(midCol.pop())
+          }
+          this.setState({midCol,leftCol,rightCol})
+        }
+
+        //To ipad from desktop
+        //shift thrid column between first two
+        else if(window.innerWidth < 1400 && this.state.width >= 1400){
+          let {leftCol, midCol, rightCol} = this.state;
+          let len = rightCol.length;
+          for(let i = 0; i < len; i++){
+            midCol.push(rightCol.pop())
+            console.log(rightCol.length)
+            if(rightCol.length){
+              leftCol.push(rightCol.pop())
+              i++;
+            }
+          }
+
+        }
       }
       this.setState({
         width: window.innerWidth
@@ -44,31 +71,55 @@ class BlogGrid extends React.Component {
       return null;
 
       function getChildElements(props) {
-        let total = 1;
-        let leftCol = [];
-        let rightCol = [];
-        props.elements.forEach((element) => {
-          if (isSelectedCategory(element)) {
-            if (total % 2) {
-              total++;
-              leftCol.push(element);
-            } else {
-              total++;
-              rightCol.push(element);
-            }
-          }
-        })
-        return {
-          leftCol,
-          rightCol,
-          currentCategory: props.currentCategory
+        if(state.width > 1400){
+          return columns(props, 3);
+        }else{
+          return columns(props, 2);
         }
-      }
 
-    function isSelectedCategory(element) {
-      return !nextProps.currentCategory || element.category.toLowerCase() === nextProps.currentCategory
+        function columns(props, columns){
+          let total = 1;
+          let leftCol = [];
+          let midCol = [];
+          let rightCol = [];
+          if(columns === 3){
+            props.elements.forEach((element) => {
+              if (isSelectedCategory(element)) {
+                if(total % 3 === 0){
+                  total++;
+                  rightCol.push(element);
+                }
+                else if (total % 2 === 0) {
+                  total++;
+                  midCol.push(element);
+                } else {
+                  total++;
+                  leftCol.push(element);
+                }
+              }
+            })
+          }
+          else{
+            props.elements.forEach((element) => {
+              if (isSelectedCategory(element)) {
+                if (total % 2 === 0) {
+                  total++;
+                  midCol.push(element);
+                } else {
+                  total++;
+                  leftCol.push(element);
+                }
+              }
+            })
+          }
+
+          return {leftCol, rightCol, midCol, currentCategory: props.currentCategory}
+        }
+
+        function isSelectedCategory(element) {
+          return !nextProps.currentCategory || element.category.toLowerCase() === nextProps.currentCategory
+        }
     }
-
   }
 
     renderCol = (elements)=>{
@@ -92,6 +143,9 @@ class BlogGrid extends React.Component {
             <div className="col-wrapper">
               <ul>
                 {this.renderCol(this.state.leftCol)}
+              </ul>
+              <ul>
+                {this.renderCol(this.state.midCol)}
               </ul>
               <ul>
                 {this.renderCol(this.state.rightCol)}
